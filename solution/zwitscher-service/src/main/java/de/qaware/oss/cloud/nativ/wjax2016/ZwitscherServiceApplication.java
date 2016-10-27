c2e84396f5391741a46414ccaf78333bf2d1f9c9
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2016-10-27 23:50:49 +0200

    Enabled Twitter configuration and fixed test using mock.

--- solution/zwitscher-service/src/main/java/de/qaware/oss/cloud/nativ/wjax2016/ZwitscherServiceApplication.java
+++ solution/zwitscher-service/src/main/java/de/qaware/oss/cloud/nativ/wjax2016/ZwitscherServiceApplication.java
@@ -1,13 +1,14 @@
 package de.qaware.oss.cloud.nativ.wjax2016;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.social.TwitterAutoConfiguration;
 
 @SpringBootApplication
-
+@EnableAutoConfiguration(exclude = {TwitterAutoConfiguration.class})
 public class ZwitscherServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ZwitscherServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZwitscherServiceApplication.class, args);
+    }
 }
--- solution/zwitscher-service/src/test/java/de/qaware/oss/cloud/nativ/wjax2016/ZwitscherServiceApplicationTests.java
+++ solution/zwitscher-service/src/test/java/de/qaware/oss/cloud/nativ/wjax2016/ZwitscherServiceApplicationTests.java
@@ -2,17 +2,31 @@ package de.qaware.oss.cloud.nativ.wjax2016;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.social.twitter.api.Twitter;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ZwitscherServiceApplicationTests.MockTwitterConfiguration.class)
 @TestPropertySource("classpath:/application-test.properties")
 public class ZwitscherServiceApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+    @Configuration
+    @Import(ZwitscherServiceApplication.class)
+    public static class MockTwitterConfiguration {
+        @Bean
+        public Twitter twitter() {
+            return Mockito.mock(Twitter.class);
+        }
     }
 
 }
