f28f1caf5da5c372e890e5292fdf26d34f6277c0
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2016-10-28 00:32:14 +0200

    Added Hystrix command to the Zwitscher repository.

--- solution/zwitscher-service/src/main/java/de/qaware/oss/cloud/nativ/wjax2016/ZwitscherRepositoryImpl.java
+++ solution/zwitscher-service/src/main/java/de/qaware/oss/cloud/nativ/wjax2016/ZwitscherRepositoryImpl.java
@@ -1,5 +1,7 @@
 package de.qaware.oss.cloud.nativ.wjax2016;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Tweet;
@@ -7,6 +9,7 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
+import java.util.Collections;
 
 import static java.util.stream.Collectors.toList;
 
@@ -24,11 +27,18 @@ public class ZwitscherRepositoryImpl implements ZwitscherRepository {
     }
 
     @Override
+    @HystrixCommand(fallbackMethod = "noResults")
+    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")
     public Collection<String> search(String q, int pageSize) {
         // search and map results
         SearchResults results = twitter.searchOperations().search(q, pageSize);
         return results.getTweets().stream()
                 .map(Tweet::getUnmodifiedText)
                 .collect(toList());
+    }
+
+    @SuppressWarnings("unused")
+    protected Collection<String> noResults(String q, int pageSize) {
+        return Collections.singleton("Error getting tweets stream, using fallback.");
     }
 }
