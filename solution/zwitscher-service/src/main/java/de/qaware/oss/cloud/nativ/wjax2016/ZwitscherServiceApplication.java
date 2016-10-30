a515101cac53787a9c76f9f28e8ed51be142354c
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2016-10-30 09:55:09 +0100

    Enabled discovery client.

--- solution/zwitscher-service/src/main/java/de/qaware/oss/cloud/nativ/wjax2016/ZwitscherServiceApplication.java
+++ solution/zwitscher-service/src/main/java/de/qaware/oss/cloud/nativ/wjax2016/ZwitscherServiceApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.social.TwitterAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableDiscoveryClient
 @EnableAutoConfiguration(exclude = {TwitterAutoConfiguration.class})
 public class ZwitscherServiceApplication {
     public static void main(String[] args) {
