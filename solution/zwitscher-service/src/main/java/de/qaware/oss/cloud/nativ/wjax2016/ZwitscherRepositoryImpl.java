8640f2125fc29e3e558554a6e1bcbeeffdad4d15
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2016-11-03 16:52:45 +0100

    Switched from list to set for collection of tweets.

--- solution/zwitscher-service/src/main/java/de/qaware/oss/cloud/nativ/wjax2016/ZwitscherRepositoryImpl.java
+++ solution/zwitscher-service/src/main/java/de/qaware/oss/cloud/nativ/wjax2016/ZwitscherRepositoryImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Collection;
 
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 
 /**
  * Default implementation of our repository to search Twitter for tweets.
@@ -29,6 +30,6 @@ public class ZwitscherRepositoryImpl implements ZwitscherRepository {
         SearchResults results = twitter.searchOperations().search(q, pageSize);
         return results.getTweets().stream()
                 .map(Tweet::getUnmodifiedText)
-                .collect(toList());
+                .collect(toSet());
     }
 }
