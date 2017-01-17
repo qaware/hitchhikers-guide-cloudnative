fea63580a4c802dd9e609d1cbddadb293a0248c7
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2017-01-17 10:04:30 +0100

    Minor corrections.

--- solution/zwitscher-board/src/test/java/de/qaware/oss/cloud/nativ/wjax2016/ZwitscherBoardApplicationTests.java
+++ solution/zwitscher-board/src/test/java/de/qaware/oss/cloud/nativ/wjax2016/ZwitscherBoardApplicationTests.java
@@ -7,26 +7,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.social.twitter.api.Twitter;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = ZwitscherServiceApplicationTests.MockTwitterConfiguration.class)
+@SpringBootTest
 @TestPropertySource("classpath:/application-test.properties")
-public class ZwitscherServiceApplicationTests {
+public class ZwitscherBoardApplicationTests {
 
     @Test
     public void contextLoads() {
-    }
-
-    @Configuration
-    @Import(ZwitscherServiceApplication.class)
-    public static class MockTwitterConfiguration {
-        @Bean
-        public Twitter twitter() {
-            return Mockito.mock(Twitter.class);
-        }
     }
 
 }
