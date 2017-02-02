377aa2e0269aafa008f21115678299e8a6d27397
Felix Obenauer <f.obenauer@gmail.com>
2017-02-02 11:04:02 +0100

    removes discovery client annotation from service

--- solution/zwitscher-service/src/main/java/de/qaware/oss/cloud/nativ/wjax2016/ZwitscherServiceApplication.java
+++ solution/zwitscher-service/src/main/java/de/qaware/oss/cloud/nativ/wjax2016/ZwitscherServiceApplication.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.social.TwitterAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableCircuitBreaker
-@EnableDiscoveryClient
 @EnableAutoConfiguration(exclude = {TwitterAutoConfiguration.class})
 public class ZwitscherServiceApplication {
     public static void main(String[] args) {
